1ca57994d590f2d223e202c2998401ccac426e13
Nizar DELLELI <nizar.delleli@gmail.com>
2021-10-05 16:16:19 +0100

    refactor the code to fit the last best practices for package development

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-from setuptools import setup, find_namespace_packages, find_packages
+from setuptools import setup
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
 
-requirements = [
-    'pycep_correios>=5.0.0',
-    'requests>=2.10.1',
-]
-
-setup(
-    name='gerador_endereco',  # Nome (não precisa ser o nome do repositório, nem de qualquer pasta...)
-    version='1.0.6',
-    author='Michel Metran',
-    author_email='michelmetran@gmail.com',
-    description='API para criação de endereços aleatórios em um município específico',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/open-dsa/gerador_endereco',
-    packages=find_packages(),
-    install_requires=requirements,
-    keywords='python, endereço aleatório, address',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Natural Language :: Portuguese',
-        'Intended Audience :: Developers',
-    ],
-)
-
-# package_dir={
-#     'scripts': 'random_address',
-# },
-# include_package_data=True,
-# package_data={
-#     'myapp': ['data/*.txt'],
-# },
-# py_modules=['gerador_endereco'],
+setup()
