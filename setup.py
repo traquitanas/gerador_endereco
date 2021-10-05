43d8af93ab0e57c920ce4512256b8488568d95d3
Michel Metran <michelmetran@gmail.com>
2021-10-04 23:30:58 -0300

    Ajuste Setups

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='gerador_endereco',
-    version='1.0.1',
+    version='1.0.2',
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
     description='API para criação de endereços aleatórios em um município específico',
