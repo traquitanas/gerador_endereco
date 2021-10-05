006d7f63f250fe472f2d19806231218458fcc075
Michel Metran <michelmetran@gmail.com>
2021-10-04 22:14:11 -0300

    Ajuste Setups

--- setup.py
+++ setup.py
@@ -12,19 +12,17 @@ requirements = [
 ]
 
 setup(
-    name='Endereços Aleatórios',
+    name='script',
     version='1.0.0',
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
     description='API para criação de endereços aleatórios em um município específico',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/open-dsa/gerador_endereco',
-    packages=[
-        'gerador_endereco',
-    ],
+    packages=['script'],
     package_dir={
-        'consulta_correios': 'consulta_correios',
+        'script': 'random_address',
     },
     install_requires=requirements,
     keywords='python endereço aleatório',
