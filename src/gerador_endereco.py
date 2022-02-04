a6d7eccd3f3dbfd197c0c7e63c691f1ccfce617b
Michel Metran <michelmetran@gmail.com>
2022-02-03 22:41:16 -0300

    Ajuste

--- src/gerador_endereco.py
+++ src/gerador_endereco.py
@@ -4,7 +4,7 @@ from pycep_correios import get_address_from_cep, WebService
 
 
 #VERSION = (1, 0, 7, 'dev0')
-VERSION = (1, 0, 9)
+VERSION = (1, 0, 10)
 __version__ = '.'.join(map(str, VERSION))
 
 
